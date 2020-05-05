5e69050c061e895f3c4c8c884bd3a64e355c77e5
Dealer <nguyenkhanhsl1997@gmail.com>
2020-05-05 07:56:49 +0700

    ezsudoku

---
+++ EZSUDOKU.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Cell {
+    Cell(int _x, int _y, int _data) {
+        x = _x;
+        y = _y;
+        data = _data;
+    }
+    int x, y, data;
+};
+
+int Map[8][8];
+
+bool Row[8][9];
+bool Column[8][9];
+int Big[2][2][9];
+
+vector<Cell> Backtrack;
+
+char Text_Output[2][4] = {"NO", "YES"};
+
+bool Is_Valid(int x, int y, int n) {
+    if (Row[y][n])
+        return false;
+    if (Column[x][n])
+        return false;
+    if (Big[y / 4][x / 4][n] >= 2)
+        return false;
+    return true;
+}
+
+bool Solve() {
+    int n, *pInt;
+    Backtrack.clear();
+    for (int i = 0; i < 8; i++) {
+        for (int j = 1; j < 9; j++)
+            Row[i][j] = Column[i][j] = false;
+    }
+    for (int i = 1; i < 9; i++)
+        Big[0][0][i] = Big[0][1][i] = Big[1][0][i] = Big[1][1][i] = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            n = Map[i][j];
+            if (n != 0) {
+                pInt = &Big[i / 4][j / 4][n];
+                (*pInt)++;
+                if (*pInt > 2 || Row[i][n] || Column[j][n])
+                    return false;
+                Row[i][n] = true;
+                Column[j][n] = true;
+            } else {
+                Backtrack.push_back(Cell(j, i, 1));
+            }
+        }
+    }
+    int Current = 0;
+    Cell *p;
+    int Size = Backtrack.size();
+    while (true) {
+        p = &Backtrack[Current];
+        while (!Is_Valid(p->x, p->y, p->data)) {
+            p->data++;
+            while (p->data == 9) {
+                Current--;
+                if (Current == -1)
+                    return false;
+                p = &Backtrack[Current];
+                Row[p->y][p->data] = false;
+                Column[p->x][p->data] = false;
+                Big[p->y / 4][p->x / 4][p->data]--;
+                p->data++;
+            }
+        }
+        Row[p->y][p->data] = true;
+        Column[p->x][p->data] = true;
+        Big[p->y / 4][p->x / 4][p->data]++;
+        Map[p->y][p->x] = p->data;
+        Current++;
+        if (Current == Size)
+            break;
+        Backtrack[Current].data = 1;
+    }
+    return true;
+}
+
+int main() {
+    freopen ("Input.txt", "r", stdin);
+
+    int Test, Value, n;
+    cin >> Test;
+    for (int k = 0; k < Test; k++) {
+        for (int i = 0; i < 8; i++)
+            for (int j = 0; j < 8; j++)
+                cin >> Map[i][j];
+        Value = Solve();
+        cout << "Test case #" << k + 1 << ": " << Text_Output[Value] << endl;
+        if (Value == 1) {
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++)
+                    cout << Map[i][j] << ' ';
+                cout << endl;
+            }
+        }
+
+    }
+
+    //fclose (stdin);
+    return 0;
+}
--- README.md
+++ README.md
@@ -12,8 +12,9 @@ Spoj Problems Solved
 - PQUEUE
 - QUE1
 - STPAR
-- DIEHARD 11:45am 02/05/2020
-- CODEM4 03:15pm 02/05/2020 but ugly code
-- INVCNT 12:15am 03/05/2020
-- ARRANGE 04:30pm 03/05/2020 funny ^_^
+- DIEHARD 11:45am 02/05/2020 Đệ quy, Quy hoạch động, Phương pháp ban đầu: sau khi chuyển trạng thái từ Air->Water(Fire), ở đây còn xét cả trường hợp chuyển tiếp sang Fire(Water), thực ra chỉ cần xét chuyển sang Air là đủ do Air cả Health và Armor đều tăng nên sẽ đã viết lại chương trình mới
+- CODEM4 03:15pm 02/05/2020 Đệ quy, Minimax, lấy max của 2 trường hợp khi lấy bên trái và bên phải nhưng lần lấy trước cũng ảnh hưởng đến lần lấy sau, cứ như vậy nên ta phải tách đến cái dãy nhỏ nhất để xét trước, nhưng tuy nhiên vẫn phải sử dụng thêm Quy hoạch động để tăng hiệu năng nếu ko sẽ bị TLE
+- INVCNT 12:15am 03/05/2020 Merge sort, lúc đầu sử dụng thuật toán tương tự Selection sort -> o(n2) chậm -> chuyển thành o(nlogn)
+- ARRANGE 04:30pm 03/05/2020 bài này khá "hài", chỉ sử dụng mỗi toán học
+- EZSUDOKU 07:45am 05/05/2020 kết hợp Quay lui + Quy hoạch động (lưu trữ)
 ```
