0dde5a7f654ce413412c96b1684d8a87cc3af5d5
Dealer <nguyenkhanhsl1997@gmail.com>
2020-05-09 22:08:37 +0700

    update is tree

--- PT07Y.cpp
+++ PT07Y.cpp
@@ -1,3 +1,5 @@
+#if 0
+
 #include <iostream>
 
 using namespace std;
@@ -30,9 +32,9 @@ int main() {
         cout << "NO";
         return 0;
     }
-    for (int i = 0; i < N; i++) {
+    for (int i = 1; i <= N; i++) {
         Mark[i] = false;
-        for (int j = 0; j < N; j++)
+        for (int j = 1; j <= N; j++)
             Map[i][j] = false;
     }
     for (int i = 0; i < M; i++) {
@@ -48,3 +50,64 @@ int main() {
         cout << "NO";
     return 0;
 }
+
+#endif // 0
+
+// Thử chuyển sang BFS cùng với việc check xem đồ thị có chu trình không nhưng tốc độ cũng chẳng cải thiện mấy
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+bool Map[10001][10001];
+bool Mark[10001];
+int M, N, Count;
+
+bool BFS(int n) {
+    Count = 0;
+    queue<int> Q;
+    Q.push(n);
+    int p;
+    while (!Q.empty()) {
+        Count++;
+        if (Count == N)
+            return true;
+        p = Q.front();
+        Q.pop();
+        for (int i = 1; i <= N; i++) {
+            if (Map[p][i]) {
+                if (Mark[i])
+                    return false;
+                Map[p][i] = Map[i][p] = false;
+                Mark[i] = true;
+                Q.push(i);
+            }
+        }
+    }
+    return false;
+}
+
+int main() {
+    int a, b;
+    cin >> N >> M;
+    if (M != N - 1) {
+        cout << "NO";
+        return 0;
+    }
+    for (int i = 1; i <= N; i++) {
+        Mark[i] = false;
+        for (int j = 1; j <= N; j++)
+            Map[i][j] = false;
+    }
+    for (int i = 1; i <= M; i++) {
+        cin >> a >> b;
+        Map[b][a] = Map[a][b] = true;
+    }
+    if (BFS(1))
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
+
